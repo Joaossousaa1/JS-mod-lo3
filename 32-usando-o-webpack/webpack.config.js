1a9974d9712ab3f89defad486f2a056f0fe0a18b
João note <joaossousaa31@gmail.com>
2024-05-04 16:22:56 -0300

    feat: teste

--- 32-usando-o-webpack/webpack.config.js
+++ 32-usando-o-webpack/webpack.config.js
@@ -2,12 +2,21 @@ const path = require('path')
 
 module.exports = {
   entry: {
-    nomeArq: './src/index.js',
-    outroArq: './src/hello.js'
+    index: './src/index.js',
   },
-  output: {
-    path: path.resolve(__dirname, 'public'), //mesma coisa que /Documents/VSCode/.../public
-    filename: '[name]bundle.min.js'
-  }, 
   mode: 'development' //tem development e production
 }
+
+//Antes de estudar loardes
+// const path = require('path')
+// module.exports = {
+//   entry: {
+//     nomeArq: './src/index.js',
+//     outroArq: './src/hello.js'
+//   },
+//   output: {
+//     path: path.resolve(__dirname, 'public'), //mesma coisa que /Documents/VSCode/.../public
+//     filename: '[name]bundle.min.js'
+//   }, 
+//   mode: 'development' //tem development e production
+// }
