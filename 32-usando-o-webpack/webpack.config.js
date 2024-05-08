f9383ca0c804f2587c9b3508a8b78129149bf0f3
João Otávio <joaossousaa31@gmail.com>
2024-05-08 16:01:59 -0300

    feat: to introduction a plugin css

--- 32-usando-o-webpack/webpack.config.js
+++ 32-usando-o-webpack/webpack.config.js
@@ -1,14 +1,24 @@
+const Path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+
 module.exports = {
   entry: {
     index: './src/index.js'
+  },
+  output: {
+    path: Path.resolve(__dirname, 'dist')
   },
   mode: 'development',
   module: {
     rules: [{
       test: /\.css$/,
-      use: ['style-loader', 'css-loader']
-    }]
-  }
+      use: [MiniCssExtractPlugin.loader, 'css-loader']
+    }]    
+  },
+  plugins: [
+    new MiniCssExtractPlugin()
+  ]
 }
 
 //Antes de estudar loardes
